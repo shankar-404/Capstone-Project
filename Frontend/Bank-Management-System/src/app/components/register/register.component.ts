8499f9bbe68581236938f98af03988e69efe0d6f
Shankar <seshankar404@gmail.com>
2022-11-06 17:45:48 +0530

    UI validation for registeration

--- Frontend/Bank-Management-System/src/app/components/register/register.component.ts
+++ Frontend/Bank-Management-System/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import ValidateForm from 'src/app/helpers/validateForm';
 
 @Component({
   selector: 'app-register',
@@ -10,10 +13,21 @@ export class RegisterComponent implements OnInit {
   passwordInputType: string = "password";
   passwordIsText: boolean = false;
   passwordIcon: string = "fa-eye-slash";
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      middleName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      city: ['', Validators.required],
+      contact: ['', Validators.required],
+      occupation: ['', Validators.required],
+      dob: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
   hideShowPassword(){
@@ -21,4 +35,16 @@ export class RegisterComponent implements OnInit {
     this.passwordIsText ? this.passwordIcon = "fa-eye" : this.passwordIcon = "fa-eye-slash";
     this.passwordIsText ? this.passwordInputType = "text" : this.passwordInputType = "password";
   }
+
+  onSubmit(){
+    if(this.registerForm.valid){
+      //API Request
+      console.log(this.registerForm.value)
+    }
+    else{
+      ValidateForm.validateFormFields(this.registerForm);
+      this.toastr.error("Invalid Form","Error");
+    }
+  }
+
 }
