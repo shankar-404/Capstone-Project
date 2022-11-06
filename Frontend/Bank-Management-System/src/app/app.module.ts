a173a4cbe0c50fe6f9bc3afdacb2ac5efce3abe2
Shankar <seshankar404@gmail.com>
2022-11-06 17:45:32 +0530

    UI validation for login

--- Frontend/Bank-Management-System/src/app/app.module.ts
+++ Frontend/Bank-Management-System/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +19,9 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Frontend/Bank-Management-System/src/app/components/login/login.component.ts
+++ Frontend/Bank-Management-System/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateForm';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +13,32 @@ export class LoginComponent implements OnInit {
   passwordInputType: string = "password";
   passwordIsText: boolean = false;
   passwordIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      customerId: ['',Validators.required],
+      password: ['',Validators.required]
+    })
   }
 
   hideShowPassword(){
     this.passwordIsText = !this.passwordIsText;
     this.passwordIsText ? this.passwordIcon = "fa-eye" : this.passwordIcon = "fa-eye-slash";
     this.passwordIsText ? this.passwordInputType = "text" : this.passwordInputType = "password";
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      //API Request
+      
+    }
+    else{
+      this.toastr.error("Invalid Form","Error");
+      ValidateForm.validateFormFields(this.loginForm);
+    }
   }
 
 }
