1b3883dfef79e47bfcc456a974f992db1cebb08a
Dwaipayan Ray <dwaipayanray1@gmail.com>
2022-11-10 11:10:46 +0530

    Link db to transactions

--- Backend/Bank-Management-System/Bank-Management-System/Services/TransactionService.cs
+++ Backend/Bank-Management-System/Bank-Management-System/Services/TransactionService.cs
@@ -1,5 +1,6 @@
 using Bank_Management_System.Database;
 using Bank_Management_System.Entities;
+using Bank_Management_System.Constants;
 using Bank_Management_System.Interfaces;
 using System.Reflection.Metadata.Ecma335;
 
@@ -14,35 +15,100 @@ namespace Bank_Management_System.Services
             context = new UserInfoDbContext();
         }
 
+        private Account getAccountFromUserId(int id)
+        {
+            var account = context.AccounList.Where(a => a.UserInfoId == id).FirstOrDefault();
+            if (account == null)
+            {
+                account = new Account { Balance = 0, UserInfoId = id };
+                context.AccounList.Add(account);
+                context.SaveChanges();
+            }
+
+            return account;
+        }
+
+        public bool validateTransaction(TransactionRequest t)
+        {
+            var account = getAccountFromUserId(t.UserId);
+
+            if (t.Type == Constants.Constants.WITHDRAW)
+            {
+                if (t.Amount > account.Balance)
+                {
+                    return false;
+                }
+            }
+                
+            return true;
+        }
+
         public TransactionResponse Withdraw(TransactionRequest t)
         {
-            return new TransactionResponse {
-                AccountId = 1,
-                UserId = 1,
-                Status = true,
-                Balance = 0
+            var account = getAccountFromUserId(t.UserId);
+
+            if (validateTransaction(t))
+            {
+                account.Balance -= t.Amount ?? 0.0;
+                context.AccounList.Update(account);
+                context.SaveChanges();
+
+                return new TransactionResponse
+                {
+                    AccountId = account.Id,
+                    UserId = account.UserInfoId,
+                    Status = true,
+                    Balance = account.Balance
+                };
+            }
+
+            return new TransactionResponse
+            {
+                AccountId = account.Id,
+                UserId = account.UserInfoId,
+                Status = false,
+                Balance = account.Balance
             };
         }
 
         public TransactionResponse Deposit(TransactionRequest t)
         {
+            var account = getAccountFromUserId(t.UserId);
+
+            if (validateTransaction(t))
+            {
+                account.Balance += t.Amount ?? 0.0;
+                context.AccounList.Update(account);
+                context.SaveChanges();
+
+                return new TransactionResponse
+                {
+                    AccountId = account.Id,
+                    UserId = account.UserInfoId,
+                    Status = true,
+                    Balance = account.Balance
+                };
+            }
+
             return new TransactionResponse
             {
-                AccountId = 1,
-                UserId = 1,
-                Status = true,
-                Balance = 0
+                AccountId = account.Id,
+                UserId = account.UserInfoId,
+                Status = false,
+                Balance = account.Balance
             };
         }
 
         public TransactionResponse Balance(int id)
         {
+            var account = getAccountFromUserId(id);
+
             return new TransactionResponse
             {
-                AccountId = 1,
-                UserId = 1,
+                AccountId = account.Id,
+                UserId = account.UserInfoId,
                 Status = true,
-                Balance = 0
+                Balance = account.Balance
             };
         }
     }
