bbead3915f26f2f6dd59d743cf49b9eaaabe4309
Shankar <seshankar404@gmail.com>
2022-11-11 17:03:25 +0530

    Minor fix

--- Backend/Bank-Management-System/Bank-Management-System/Database/UserInfoDbContext.cs
+++ Backend/Bank-Management-System/Bank-Management-System/Database/UserInfoDbContext.cs
@@ -8,10 +8,10 @@ namespace Bank_Management_System.Database
     public class UserInfoDbContext:DbContext
     {
         //Monish-ConnectionString
-        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=BankDb;Trusted_Connection=True;trustServerCertificate=true;";
+        //private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=BankDb;Trusted_Connection=True;trustServerCertificate=true;";
 
         //Shankar-ConnectionString
-        //private const string ConnectionString = @"server=DESKTOP-TSPSRDD;database=BankDb;Trusted_Connection=true;trustServerCertificate=true;";
+        private const string ConnectionString = @"server=DESKTOP-TSPSRDD;database=BankDb;Trusted_Connection=true;trustServerCertificate=true;";
 
         public DbSet<UserInfo> UsersList { get; set; }
         public DbSet<Account> AccountList { get; set; }
